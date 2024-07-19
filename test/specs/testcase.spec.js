b65cf7f741a5bff68894ef620651779c22d4b18c
WBL-Training <training@whitebox-learning.com>
2024-07-19 06:01:09 +0530

    Update testcase.spec.js

--- test/specs/testcase.spec.js
+++ test/specs/testcase.spec.js
@@ -1,55 +1,36 @@
-// describe("linkedin", ()=>{
-//     it("login",async ()=>{
-//         browser.url("https://www.linkedin.com/login/")
-//         browser.pause(5000)
-//         await $('[name="session_key"]').setValue("srikanthprabha62@gmail.com")
-//         browser.pause(5000)
-//         await $('[id="password"]').setValue("Rajan@91")
-//         browser.pause(5000)
-//         await $('[class="btn__primary--large from__button--floating"]').click()
-//         browser.pause(5000)
-//         await $('[class="search-global-typeahead__input"]').click().clearValue().setValue("frontend developer")
-//         browser.keys('Enter')
-//     })
-// })
 
-
-
-
-// login.spec.js
 describe("LinkedIn", () => {
-    it("should login and search for a job", async () => {
-        // Open LinkedIn login page
-        await browser.url("https://www.linkedin.com/login/");
+  it("should login and search for a job", async () => {
+    await browser.url("https://www.linkedin.com/login/");
 
-        // Pause to ensure the page is fully loaded
-        await browser.pause(5000);
+    await browser.pause(5000);
 
-        // Set email value
-        await $('[name="session_key"]').setValue("Enter_email");
+    await $('[name="session_key"]').setValue("srikanthprabha62@gmail.com");
 
-        // Pause to simulate user interaction delay
-        await browser.pause(5000);
+    await browser.pause(5000);
 
-        // Set password value
-        await $('[id="password"]').setValue("Enter_password");
+    await $('[id="password"]').setValue("Rajan@91");
 
-        // Pause to simulate user interaction delay
-        await browser.pause(5000);
+    await browser.pause(5000);
 
-        // Click on the login button
-        await $('[class="btn__primary--large from__button--floating"]').click();
+    await $('[class="btn__primary--large from__button--floating"]').click();
 
-        // Pause to simulate wait time after login
-        await browser.pause(5000);
+    await browser.pause(5000);
 
-        // Click on the search input and set the value
-        const searchInput = await $('[class="search-global-typeahead__input"]');
-        await searchInput.click();
-        await searchInput.clearValue();
-        await searchInput.setValue("frontend developer");
+    const searchInput = await $('[class="search-global-typeahead__input"]');
+    await searchInput.click();
+    await searchInput.clearValue();
+    await searchInput.setValue("frontend developer");
 
-        // Press Enter to search
-        await browser.keys('Enter');
-    });
+    await browser.keys("Enter");
+
+    const location = await $(
+      '[class="jobs-search-box__text-input jobs-search-box__text-input--with-clear"]'
+    );
+    await location.click();
+    await location.clearValue();
+    await location.setValue("USA");
+
+    await browser.keys("Enter");
+  });
 });
